924628e9768ae8a45a25075ae6efcf24f4efa2cd
Paul Rogers <paul@dingosky.com>
2024-10-27 13:56:23 +0100

    fix autoExpand on select concept

--- src/components/kb/panels/concepts/tree/TaxonomyTree.jsx
+++ src/components/kb/panels/concepts/tree/TaxonomyTree.jsx
@@ -51,33 +51,36 @@ const TaxonomyTree = ({ concept, selectConcept, taxonomy }) => {
       if (expandedItems.length === 0) {
         setExpandedItems(getConceptPath(taxonomy, concept))
       } else {
-        const isExpanded = expandedItems.includes(concept.name)
-        const expand = autoExpand ? !isExpanded : isExpanded
-        expandConcept(concept, expand)
-      }
-
-      if (timeoutRef.current) {
-        clearTimeout(timeoutRef.current)
-      }
-
-      // Scroll and focused item after a short delay to allow tree expansion
-      timeoutRef.current = setTimeout(() => {
-        const domElement = apiRef.current.getItemDOMElement(concept.name)
-        if (!domElement) {
-          return
+        if (autoExpand && !expandedItems.includes(concept.name)) {
+          expandConcept(concept, true)
+          setAutoExpand(false)
         }
-        const rect = domElement.getBoundingClientRect()
-        const conceptIsVisible =
-          rect.top >= 0 &&
-          rect.bottom <=
-            (window.innerHeight || document.documentElement.clientHeight)
-        if (!conceptIsVisible) {
-          domElement.scrollIntoView({ behavior: "smooth", block: "nearest" })
-          apiRef.current.focusItem(null, concept.name)
-        }
-      }, 500)
+      }
     }
-  }, [concept, taxonomy])
+  }, [autoExpand, concept, expandConcept, expandedItems, taxonomy])
+
+  useEffect(() => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current)
+    }
+
+    // Scroll and focused item after a short delay to allow tree expansion
+    timeoutRef.current = setTimeout(() => {
+      const domElement = apiRef.current.getItemDOMElement(concept.name)
+      if (!domElement) {
+        return
+      }
+      const rect = domElement.getBoundingClientRect()
+      const conceptIsVisible =
+        rect.top >= 0 &&
+        rect.bottom <=
+          (window.innerHeight || document.documentElement.clientHeight)
+      if (!conceptIsVisible) {
+        domElement.scrollIntoView({ behavior: "smooth", block: "nearest" })
+        apiRef.current.focusItem(null, concept.name)
+      }
+    }, 500)
+  }, [apiRef, concept])
 
   useEffect(() => {
     const arrowKeys = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"]
