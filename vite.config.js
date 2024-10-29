739ad95f4fcb5df93eb4ded322f029d75e972f71
Paul Rogers <paul@dingosky.com>
2024-10-29 15:45:47 +0100

    stub vite proxy server options

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,26 @@
-import path from "path"
-import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import path from "path"
+import { fileURLToPath } from "url"
+import { defineConfig } from "vite"
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://your-proxy-server.com",
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ""),
+      },
     },
   },
 })
