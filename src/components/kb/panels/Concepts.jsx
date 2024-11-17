bda0316e8bf2aa3c5354c83f082f93a127d15690
Paul Rogers <paul@dingosky.com>
2024-11-17 10:13:19 +0100

    concept sidebar divider

--- src/components/kb/panels/Concepts.jsx
+++ src/components/kb/panels/Concepts.jsx
@@ -1,14 +1,13 @@
-import { use, useEffect } from "react"
+import { use, useState } from "react"
 
 import Box from "@mui/material/Box"
-import Divider from "@mui/material/Divider"
-import { useCallback, useState } from "react"
 
 import Concept from "@/components/kb/panels/concepts/Concept"
 import TaxonomySidebar from "@/components/kb/panels/concepts/TaxonomySidebar"
 
 import SelectedContext from "@/contexts/selected/SelectedContext"
 import TaxonomyContext from "@/contexts/taxonomy/TaxonomyContext"
+import ConceptDivider from "./concepts/ConceptDivider"
 
 const Concepts = () => {
   const { getConcept } = use(TaxonomyContext)
@@ -16,45 +15,7 @@ const Concepts = () => {
 
   const concept = getConcept(selected.concept)
 
-  const [sidebarWidth, setSidebarWidth] = useState(320)
-  const [isDragging, setIsDragging] = useState(false)
-
-  const handleMouseDown = useCallback(() => {
-    setIsDragging(true)
-  }, [])
-
-  const handleMouseMove = useCallback(
-    event => {
-      if (isDragging) {
-        const newWidth = Math.min(Math.max(event.clientX, 320), 500)
-        setSidebarWidth(newWidth)
-      }
-    },
-    [isDragging]
-  )
-
-  const handleMouseUp = useCallback(() => {
-    setIsDragging(false)
-  }, [])
-
-  const handleDoubleClick = useCallback(() => {
-    setSidebarWidth(prevWidth => (prevWidth < 325 ? 500 : 320))
-  }, [])
-
-  useEffect(() => {
-    if (isDragging) {
-      window.addEventListener("mousemove", handleMouseMove)
-      window.addEventListener("mouseup", handleMouseUp)
-    } else {
-      window.removeEventListener("mousemove", handleMouseMove)
-      window.removeEventListener("mouseup", handleMouseUp)
-    }
-
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove)
-      window.removeEventListener("mouseup", handleMouseUp)
-    }
-  }, [isDragging, handleMouseMove, handleMouseUp])
+  const [sidebarWidth, setSidebarWidth] = useState(0)
 
   if (!concept) {
     return null
@@ -65,13 +26,7 @@ const Concepts = () => {
       <Box sx={{ width: sidebarWidth }}>
         <TaxonomySidebar concept={concept} />
       </Box>
-      <Divider
-        flexItem
-        orientation="vertical"
-        sx={{ borderRightWidth: 6, cursor: "col-resize" }}
-        onMouseDown={handleMouseDown}
-        onDoubleClick={handleDoubleClick}
-      />
+      <ConceptDivider setSidebarWidth={setSidebarWidth} />
       <Box sx={{ flexGrow: 1, overflowY: "auto", pl: 1 }}>
         <Concept concept={concept} />
       </Box>
---
+++ src/components/kb/panels/concepts/ConceptDivider.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useCallback, useState } from "react"
+import Divider from "@mui/material/Divider"
+
+const MIN_WIDTH = 350
+const MAX_WIDTH = 750
+
+const ConceptDivider = ({ setSidebarWidth }) => {
+  const [isDragging, setIsDragging] = useState(false)
+
+  const handleMouseDown = useCallback(() => {
+    setIsDragging(true)
+  }, [])
+
+  const handleMouseMove = useCallback(
+    event => {
+      if (isDragging) {
+        const newWidth = Math.min(Math.max(event.clientX, MIN_WIDTH), MAX_WIDTH)
+        setSidebarWidth(newWidth)
+      }
+    },
+    [isDragging, setSidebarWidth]
+  )
+
+  const handleMouseUp = useCallback(() => {
+    setIsDragging(false)
+  }, [])
+
+  const handleDoubleClick = useCallback(() => {
+    setSidebarWidth(prevWidth =>
+      prevWidth < MIN_WIDTH + 5 ? MAX_WIDTH : MIN_WIDTH
+    )
+  }, [setSidebarWidth])
+
+  useEffect(() => {
+    if (isDragging) {
+      window.addEventListener("mousemove", handleMouseMove)
+      window.addEventListener("mouseup", handleMouseUp)
+    } else {
+      window.removeEventListener("mousemove", handleMouseMove)
+      window.removeEventListener("mouseup", handleMouseUp)
+    }
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove)
+      window.removeEventListener("mouseup", handleMouseUp)
+    }
+  }, [isDragging, handleMouseMove, handleMouseUp])
+
+  useEffect(() => {
+    setSidebarWidth(MIN_WIDTH)
+  }, [setSidebarWidth])
+
+  return (
+    <Divider
+      flexItem
+      orientation="vertical"
+      onMouseDown={handleMouseDown}
+      onDoubleClick={handleDoubleClick}
+      sx={{ borderRightWidth: 6, cursor: "col-resize" }}
+    />
+  )
+}
+
+export default ConceptDivider
