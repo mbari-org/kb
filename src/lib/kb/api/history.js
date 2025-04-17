5526610783e7e83726d633e11c11e1c353a6a9e2
Paul Rogers <paul@dingosky.com>
2025-04-17 08:51:27 -0600

    CxTmp bypass fetch approved history error

--- src/lib/kb/api/history.js
+++ src/lib/kb/api/history.js
@@ -2,7 +2,14 @@ import { oniGet, oniPut } from '@/lib/services/oni/methods'
 
 const fetchHistory = async (config, action) => {
   const { error, payload } = await oniGet(config, ['history', action])
-  return { error, payload: payload?.content }
+
+  // CxInc Tmp bypass history error
+  if (error) {
+    console.log('CxTmp bypassing history error', error)
+    return { payload: [] }
+  }
+
+  return { payload: payload?.content }
 }
 
 const sendPendingAction = async (config, action, pendingId) =>
