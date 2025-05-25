6b247538997e27b7f1570f0583f2175103546e5c
Paul Rogers <paul@dingosky.com>
2025-05-25 17:42:14 +0200

    make TaxonomySearch a common component

--- src/components/common/ConceptSearch.jsx
+++ src/components/common/ConceptSearch.jsx
@@ -1,52 +1,25 @@
-import { use, useEffect, useMemo, useState } from 'react'
-
+import { useEffect, useMemo, useState, use } from 'react'
 import { Stack, Typography } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
-
 import Autocomplete from '@mui/material/Autocomplete'
 import TextField from '@mui/material/TextField'
-
-import ConceptContext from '@/contexts/concept/ConceptContext'
-import SelectedContext from '@/contexts/selected/SelectedContext'
 import TaxonomyContext from '@/contexts/taxonomy/TaxonomyContext'
 
-const TaxonomySearch = ({ setAutoExpand }) => {
+const ConceptSearch = ({ conceptName, handleConceptSelect, handleKeyUp }) => {
   const theme = useTheme()
-
-  const { concept } = use(ConceptContext)
-  const { select } = use(SelectedContext)
-  const { getConceptPrimaryName, getNames } = use(TaxonomyContext)
-
   const [value, setValue] = useState('')
+  const { getConceptPrimaryName, getNames } = use(TaxonomyContext)
 
   const taxonomyNames = useMemo(() => getNames(), [getNames])
 
-  const handleConceptChange = (_event, selectedName) => {
-    if (selectedName) {
-      setAutoExpand({ expand: true, name: selectedName })
-      select({ concept: selectedName })
-    }
-  }
-
-  const handleKeyUp = event => {
-    if (event.key === 'Enter') {
-      const conceptName = event.target.value.trim()
-      if (taxonomyNames.includes(conceptName)) {
-        setAutoExpand({ expand: true, name: conceptName })
-        select({ concept: conceptName })
-        document.activeElement.blur()
-      }
-    }
-  }
-
   useEffect(() => {
-    const primaryName = getConceptPrimaryName(concept?.name)
+    const primaryName = getConceptPrimaryName(conceptName)
     setValue(primaryName || '')
-  }, [concept, getConceptPrimaryName])
+  }, [conceptName, getConceptPrimaryName])
 
   return (
     <Autocomplete
-      onChange={handleConceptChange}
+      onChange={handleConceptSelect}
       options={taxonomyNames}
       renderInput={params => (
         <Stack>
@@ -63,7 +36,7 @@ const TaxonomySearch = ({ setAutoExpand }) => {
             sx={{
               backgroundColor: theme.palette.primary.pale,
             }}
-            onKeyUp={handleKeyUp}
+            onKeyUp={event => handleKeyUp(event, taxonomyNames)}
           />
           <hr />
         </Stack>
@@ -85,4 +58,4 @@ const TaxonomySearch = ({ setAutoExpand }) => {
   )
 }
 
-export default TaxonomySearch
+export default ConceptSearch
--- src/components/kb/panels/concept/TaxonomySidebar.jsx
+++ src/components/kb/panels/concept/TaxonomySidebar.jsx
@@ -1,20 +1,33 @@
-import { useRef, useState } from 'react'
+import { useRef, useState, use } from 'react'
 
 import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
 
-import TaxonomySearch from '@/components/kb/panels/concept/TaxonomySearch'
+import ConceptSearch from '@/components/common/ConceptSearch'
 import TaxonomyTree from '@/components/kb/panels/concept/tree/TaxonomyTree'
+
+import useConceptSelect from '@/components/kb/panels/concept/useConceptSelect'
+
+import ConceptContext from '@/contexts/concept/ConceptContext'
 
 const TaxonomySidebar = () => {
   const sidebarRef = useRef(null)
 
   const [autoExpand, setAutoExpand] = useState(null)
 
+  const { concept } = use(ConceptContext)
+  const { handleConceptSelect, handleKeyUp } = useConceptSelect(setAutoExpand)
+
+  if (!concept) return null
+
   return (
     <Stack sx={{ height: '100%' }}>
       <Box sx={{ ml: 1, mt: 1, mr: 1 }}>
-        <TaxonomySearch setAutoExpand={setAutoExpand} />
+        <ConceptSearch
+          conceptName={concept.name}
+          handleConceptSelect={handleConceptSelect}
+          handleKeyUp={handleKeyUp}
+        />
       </Box>
       <Box
         ref={sidebarRef}
