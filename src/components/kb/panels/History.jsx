8eba95cd5da3bf478125bf7023dc307f5a7070ee
Paul Rogers <paul@dingosky.com>
2025-05-25 14:31:04 +0200

    separate component for pending|approved history

--- src/components/kb/panels/History.jsx
+++ src/components/kb/panels/History.jsx
@@ -1,64 +1,31 @@
 import { useState } from 'react'
-import { Typography, ToggleButtonGroup, Box } from '@mui/material'
-import { DataGrid } from '@mui/x-data-grid'
+import { ToggleButtonGroup, Box } from '@mui/material'
 
 import HistoryToggleButton from '@/components/kb/panels/history/HistoryToggleButton'
-import useHistoryColumns from '@/components/kb/panels/history/useHistoryColumns'
-import useHistoryCount from '@/components/kb/panels/history/useHistoryCount'
-
-import useLoadHistory from '@/components/kb/panels/history/useLoadHistory'
+import TypeHistory from '@/components/kb/panels/history/TypeHistory'
 
 const History = () => {
   const [type, setType] = useState('pending')
-
-  const pendingHistory = useLoadHistory('pending')
-  const approvedHistory = useLoadHistory('approved')
-  const history = type === 'pending' ? pendingHistory : approvedHistory
-
-  const columns = useHistoryColumns({ type })
-  const count = useHistoryCount(type)
 
   const handleTypeChange = (_, newType) => !!newType && setType(newType)
 
   return (
     <Box sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-      <Typography align='center' sx={{ mt: 3, mb: 1 }} variant='h3'>
-        History
-      </Typography>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 2, mt: -2 }}>
-        <Typography sx={{ ml: 2, mt: -2 }}>Total: {count}</Typography>
+      <Box sx={{ display: 'flex', justifyContent: 'flex-end', mb: 2, mt: 2 }}>
         <ToggleButtonGroup
           value={type}
           exclusive
           onChange={handleTypeChange}
           aria-label='history type'
           size='small'
-          sx={{ mr: 2, mt: -2 }}
+          sx={{ mr: 2 }}
         >
           <HistoryToggleButton value='pending' />
           <HistoryToggleButton value='approved' />
         </ToggleButtonGroup>
       </Box>
-      <Box sx={{ flexGrow: 1, minHeight: 0 }}>
-        <DataGrid
-          rows={history.history}
-          columns={columns}
-          pageSize={5}
-          rowsPerPageOptions={[5]}
-          disableSelectionOnClick
-          sx={{
-            height: '100%',
-            '& .MuiDataGrid-columnHeaders': {
-              backgroundColor: 'background.paper',
-              '& .MuiDataGrid-columnHeader': {
-                '& .MuiDataGrid-columnHeaderTitle': {
-                  fontWeight: 700,
-                  fontSize: '1rem',
-                },
-              },
-            },
-          }}
-        />
+      <Box sx={{ flexGrow: 1, minHeight: 0, mt: -8 }}>
+        <TypeHistory type={type} />
       </Box>
     </Box>
   )
---
+++ src/components/kb/panels/history/TypeHistory.jsx
@@ -0,0 +1,46 @@
+import { Typography, Box } from '@mui/material'
+import { DataGrid } from '@mui/x-data-grid'
+
+import useHistoryColumns from './useHistoryColumns'
+import useHistoryCount from './useHistoryCount'
+import useLoadHistory from './useLoadHistory'
+
+const TypeHistory = ({ type }) => {
+  const history = useLoadHistory(type)
+  const columns = useHistoryColumns({ type })
+  const count = useHistoryCount(type)
+
+  return (
+    <Box sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+      <Typography align='center' sx={{ mt: 3, mb: 1 }} variant='h3'>
+        {type === 'pending' ? 'Pending History' : 'Approved History'}
+      </Typography>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 2, mt: -2 }}>
+        <Typography sx={{ ml: 2, mt: -2 }}>Total: {count}</Typography>
+      </Box>
+      <Box sx={{ flexGrow: 1, minHeight: 0 }}>
+        <DataGrid
+          rows={history.history}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          disableSelectionOnClick
+          sx={{
+            height: '100%',
+            '& .MuiDataGrid-columnHeaders': {
+              backgroundColor: 'background.paper',
+              '& .MuiDataGrid-columnHeader': {
+                '& .MuiDataGrid-columnHeaderTitle': {
+                  fontWeight: 700,
+                  fontSize: '1rem',
+                },
+              },
+            },
+          }}
+        />
+      </Box>
+    </Box>
+  )
+}
+
+export default TypeHistory
