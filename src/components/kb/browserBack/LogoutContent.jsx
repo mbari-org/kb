e375d2dcd16013e7901e03ee44644f411b4a6d7d
Paul Rogers <paul@dingosky.com>
2025-06-06 15:43:49 +0200

    Browser back Logout message

--- src/components/kb/browserBack/LogoutContent.jsx
+++ src/components/kb/browserBack/LogoutContent.jsx
@@ -5,7 +5,18 @@ const LogoutContent = () => (
     <Typography variant='body1' color='text.secondary'>
       The browser back button closes the Knowledge Base app and logs you out.
     </Typography>
+    <Typography variant='body1' color='text.secondary' sx={{ mt: 2 }}>
+      You can use the &lt; and &gt; navigation buttons to return to a previous Concept or Panel.
+    </Typography>
     <Typography variant='body1' color='text.secondary'>
+      Concept buttons are on the Concept selection control and Panel buttons are in the top
+      navigation bar.
+    </Typography>
+    <Typography variant='body1' color='text.secondary' sx={{ mt: 2 }}>
+      Each of these buttons provides a long press option to jump to a specific point in the history
+      as well.
+    </Typography>
+    <Typography variant='body1' color='text.secondary' sx={{ mt: 5 }}>
       Are you sure you want to logout?
     </Typography>
   </Stack>
