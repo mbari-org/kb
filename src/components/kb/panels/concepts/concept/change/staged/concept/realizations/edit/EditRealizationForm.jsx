c90a8500300cc75a5eb0dce8889dca43e0cb0ec0
Paul Rogers <paul@dingosky.com>
2025-07-07 14:29:13 +0200

    realization edit form styling

--- src/components/kb/panels/concepts/concept/change/staged/concept/realizations/edit/EditRealizationForm.jsx
+++ src/components/kb/panels/concepts/concept/change/staged/concept/realizations/edit/EditRealizationForm.jsx
@@ -1,4 +1,4 @@
-import { Box, FormControl, TextField, Divider } from '@mui/material'
+import { Box, FormControl, TextField, Divider, Stack } from '@mui/material'
 
 import ToConceptSelect from '@/components/common/concept/ToConceptSelect'
 import RealizationTemplatesFilter from './filter/RealizationTemplatesFilter'
@@ -16,28 +16,36 @@ const EditRealizationForm = ({
     <Box component='form' id={EDIT_REALIZATION_FORM_ID} onSubmit={stageChange}>
       <RealizationTemplatesFilter />
       <Divider sx={{ my: 2 }} />
-      <FormControl fullWidth margin='normal'>
-        <TextField
-          label='Link Name'
-          name='linkName'
-          onChange={handleChange}
-          required
-          value={formRealizationItem.linkName}
-        />
-      </FormControl>
-      <FormControl fullWidth margin='normal'>
-        <ToConceptSelect
-          conceptName={formRealizationItem.toConcept}
-          doConceptSelected={handleToConceptSelect}
-          onSpecialChange={handleToConceptSpecial}
-        />
-      </FormControl>
+      <Stack direction='row' spacing={1} width='100%' alignItems='center'>
+        <Box sx={{ width: 200, flexShrink: 0, pt: 2.5 }}>
+          <FormControl fullWidth margin='normal'>
+            <TextField
+              label='Link Name'
+              name='linkName'
+              onChange={handleChange}
+              required
+              size='small'
+              value={formRealizationItem.linkName}
+            />
+          </FormControl>
+        </Box>
+        <Box sx={{ flex: 1 }}>
+          <FormControl fullWidth margin='normal'>
+            <ToConceptSelect
+              conceptName={formRealizationItem.toConcept}
+              doConceptSelected={handleToConceptSelect}
+              onSpecialChange={handleToConceptSpecial}
+            />
+          </FormControl>
+        </Box>
+      </Stack>
       <FormControl fullWidth margin='normal'>
         <TextField
           label='Link Value'
           name='linkValue'
           onChange={handleChange}
           required
+          size='small'
           value={formRealizationItem.linkValue}
         />
       </FormControl>
