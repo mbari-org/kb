bdbadcaeff6bad609377694662af4ee7bdae3085
Paul Rogers <paul@dingosky.com>
2025-07-11 14:44:22 +0200

    cleanup

--- src/components/kb/panels/concepts/concept/change/staged/realizations/edit/RealizationContent.jsx
+++ src/components/kb/panels/concepts/concept/change/staged/realizations/edit/RealizationContent.jsx
@@ -1,4 +1,4 @@
-import { use, useState, useMemo } from 'react'
+import { use, useState, useMemo, useEffect } from 'react'
 import { Box, Divider } from '@mui/material'
 
 import RealizationForm from '@/components/kb/panels/concepts/concept/change/staged/realizations/edit/form/RealizationForm'
@@ -9,11 +9,10 @@ import ConceptModalContext from '@/contexts/panels/concepts/modal/ConceptModalCo
 import PanelDataContext from '@/contexts/panelData/PanelDataContext'
 
 import { EMPTY_TEMPLATE } from '@/lib/kb/model/template'
-import { isSame } from '@/lib/kb/model/realization'
+import { checkForDuplicate } from '@/lib/kb/model/realization'
 
 import useStageRealization from './useStageRealization'
-
-import useDebounce from '@/hooks/useDebounce'
+import useRealizationContentHandlers from './useRealizationContentHandlers'
 
 import { CONCEPT_STATE } from '@/lib/constants'
 
@@ -27,84 +26,29 @@ const RealizationContent = () => {
   const [realizationItem, setRealizationItem] = useState(modalRealizationItem || EMPTY_TEMPLATE)
 
   const isDuplicate = useMemo(() => {
-    // Don't check for duplicates if form is incomplete
-    if (!realizationItem.linkName || !realizationItem.toConcept || !realizationItem.linkValue) {
-      return false
-    }
-
-    // For editing existing realizations, exclude the current realization from the check
-    const realizationsToCheck =
-      action === CONCEPT_STATE.REALIZATION_ITEM.ADD
-        ? stagedState.realizations
-        : stagedState.realizations.filter((_, index) => index !== realizationIndex)
-
-    return realizationsToCheck.some(existing => isSame(realizationItem, existing))
+    const excludeIndex = action === CONCEPT_STATE.REALIZATION_ITEM.ADD ? null : realizationIndex
+    return checkForDuplicate(stagedState.realizations, realizationItem, excludeIndex)
   }, [realizationItem, stagedState.realizations, action, realizationIndex])
 
-  // Update modal context with duplicate state
-  useMemo(() => {
+  useEffect(() => {
     setModalData(prev => ({
       ...prev,
       isDuplicate,
     }))
   }, [isDuplicate, setModalData])
 
-  const debouncedInput = useDebounce((updatedRealizationItem, fieldIsModified, field) => {
-    const updatedModified = { ...modalData.modified, [field]: fieldIsModified }
+  const stageChange = useStageRealization()
 
-    setModalData(prev => ({
-      ...prev,
-      realizationItem: updatedRealizationItem,
-      modified: updatedModified,
-    }))
-  }, 300)
-
-  const handleRealizationChange = (updatedRealizationItem, field) => {
-    setRealizationItem(updatedRealizationItem)
-
-    const fieldIsModified =
-      action === CONCEPT_STATE.REALIZATION_ITEM.ADD
-        ? updatedRealizationItem[field] !== EMPTY_TEMPLATE[field]
-        : stagedState.realizations[realizationIndex][field] !== updatedRealizationItem[field]
-
-    debouncedInput(updatedRealizationItem, fieldIsModified, field)
-  }
-
-  const handleTemplateSelect = template => {
-    // Auto-populate all fields from the selected template
-    const updatedRealizationItem = {
-      ...realizationItem,
-      linkName: template.linkName,
-      toConcept: template.toConcept,
-      linkValue: template.linkValue,
-      templateId: template.id, // Store the template ID for reference
-    }
-
-    setRealizationItem(updatedRealizationItem)
-
-    // Mark all populated fields as modified
-    const updatedModified = {
-      ...modalData.modified,
-      linkName: true,
-      toConcept: true,
-      linkValue: true,
-    }
-
-    setModalData(prev => ({
-      ...prev,
-      realizationItem: updatedRealizationItem,
-      modified: updatedModified,
-    }))
-  }
-
-  const stageRealization = useStageRealization()
-  const stageChange = event => {
-    if (isDuplicate) {
-      event.preventDefault()
-      return
-    }
-    stageRealization(event)
-  }
+  const { handleRealizationChange, handleTemplateSelect } = useRealizationContentHandlers({
+    action,
+    modalData,
+    realizationIndex,
+    realizationItem,
+    setModalData,
+    setRealizationItem,
+    stagedState,
+    stageChange,
+  })
 
   if (!realizationItem) {
     return null
@@ -119,8 +63,8 @@ const RealizationContent = () => {
       />
       <Divider sx={{ my: 1 }} />
       <RealizationForm
-        realizationItem={realizationItem}
         onRealizationChange={handleRealizationChange}
+        realizationItem={realizationItem}
         stageChange={stageChange}
       />
     </Box>
