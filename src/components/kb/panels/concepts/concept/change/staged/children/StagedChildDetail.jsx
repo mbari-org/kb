ea2a7a6298718f8efe0bdb49a4035d732e613ac2
Paul Rogers <paul@dingosky.com>
2025-07-28 18:42:37 +0200

    complete StagedChildDetail

--- src/components/kb/panels/concepts/concept/change/staged/children/StagedChildDetail.jsx
+++ src/components/kb/panels/concepts/concept/change/staged/children/StagedChildDetail.jsx
@@ -1,5 +1,24 @@
-const StagedChildDetail = ({ initialChild, stagedChild }) => {
-  return <div>StagedChildDetail</div>
+import { useMemo } from 'react'
+import { Box } from '@mui/material'
+
+import FieldValueDisplay from '@/components/common/FieldValueDisplay'
+
+import { drop } from '@/lib/utils'
+
+const StagedChildDetail = ({ stagedChild }) => {
+  const { updates } = stagedChild
+
+  const fieldValues = useMemo(() => {
+    return Object.entries(drop(updates, ['name']))
+  }, [updates])
+
+  return (
+    <Box>
+      {fieldValues?.map(([field, value]) => (
+        <FieldValueDisplay key={field} field={field} value={value} />
+      ))}
+    </Box>
+  )
 }
 
 export default StagedChildDetail
