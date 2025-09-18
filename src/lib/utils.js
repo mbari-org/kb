4671c411b4dcf3896d0df1fa5f235f10a59f86f9
Paul Rogers <paul@dingosky.com>
2025-09-18 16:35:40 +0200

    Fixed length human date timestamp text

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -96,12 +96,12 @@ const humanTimestamp = timestamp => {
   }
 
   const month = date.toLocaleString('default', { month: 'short' })
-  const day = date.getDate()
+  const day = date.getDate().toString().padStart(2, '0')
   const year = date.getFullYear()
   const hours = date.getHours().toString().padStart(2, '0')
   const minutes = date.getMinutes().toString().padStart(2, '0')
 
-  return `${month} ${day}, ${year} ${hours}:${minutes}`
+  return `${month} ${day} ${year} ${hours}:${minutes}`
 }
 
 const isDeepEqual = (obj1, obj2, depth = Infinity) => {
