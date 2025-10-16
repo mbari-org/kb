299cee415b07ed7c1907cc4aa3f280f5b2a189f6
Paul Rogers <paul@dingosky.com>
2025-10-16 10:44:39 +0200

    toggle references by concept on clear

--- src/components/kb/panels/references/header/ReferencesHeaderLeft.jsx
+++ src/components/kb/panels/references/header/ReferencesHeaderLeft.jsx
@@ -8,11 +8,11 @@ import { CONCEPT_SELECT, SELECTED } from '@/lib/constants'
 
 const { NAV_HISTORY } = CONCEPT_SELECT.RIGHT_COMPONENT
 
-const { CONCEPT } = SELECTED
-const { REFERENCES } = SELECTED.SETTINGS
+const { CONCEPT, SETTINGS } = SELECTED
+const { REFERENCES } = SETTINGS
 
 const ReferencesHeaderLeft = () => {
-  const { getSelected, getSettings, updateSelected } = use(SelectedContext)
+  const { getSelected, getSettings, updateSelected, updateSettings } = use(SelectedContext)
 
   const selectedConcept = getSelected(CONCEPT)
   const byConcept = getSettings(REFERENCES.KEY, REFERENCES.BY_CONCEPT)
@@ -23,11 +23,16 @@ const ReferencesHeaderLeft = () => {
     }
   }
 
+  const handleClear = () => {
+    updateSettings({ [REFERENCES.KEY]: { [REFERENCES.BY_CONCEPT]: false } })
+  }
+
   return (
     <ConceptSelect
       conceptName={selectedConcept}
       disabled={!byConcept}
       doConceptSelected={handleConceptSelect}
+      onClear={handleClear}
       rightComponent={NAV_HISTORY}
     />
   )
