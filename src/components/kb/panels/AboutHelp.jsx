7a89a81553916073239b016d2655e5545aa4d5e2
Paul Rogers <paul@dingosky.com>
2026-01-14 12:26:15 +0100

    fix About/Help version display

--- src/components/kb/panels/AboutHelp.jsx
+++ src/components/kb/panels/AboutHelp.jsx
@@ -1,4 +1,8 @@
-import EmptyPanel from '@/components/common/panel/EmptyPanel'
+import { Box } from '@mui/material'
+
+import PanelHeaderTitle from '@/components/common/panel/PanelHeaderTitle'
+import usePanelFactory from '@/components/common/panel/usePanelFactory'
+
 import CONFIG from '@/text'
 import useVersionTooltip from '@/lib/hooks/useVersionTooltip'
 import { getVersion } from '@/version'
@@ -8,8 +12,15 @@ const TITLE = CONFIG.PANELS.ABOUT_HELP.TITLE
 const AboutHelp = () => {
   const version = getVersion()
   const versionTooltip = useVersionTooltip()
+  const { createPanelHeader } = usePanelFactory()
 
-  return <EmptyPanel title={TITLE} subtitle={`v${version}`} subtitleTooltip={versionTooltip} />
+  const header = createPanelHeader({
+    headerTitle: (
+      <PanelHeaderTitle subtitle={`v${version}`} subtitleTooltip={versionTooltip} title={TITLE} />
+    ),
+  })
+
+  return <Box sx={{ mt: 1 }}>{header}</Box>
 }
 
 export default AboutHelp
