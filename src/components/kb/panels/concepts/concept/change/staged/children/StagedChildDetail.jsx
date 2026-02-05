ca340392935db6a1764c55d255edd50c8319b4eb
Paul Rogers <paul@dingosky.com>
2026-02-05 16:04:47 +0100

    suppress Staged edit rank if not marine organism

--- src/components/kb/panels/concepts/concept/change/staged/children/StagedChildDetail.jsx
+++ src/components/kb/panels/concepts/concept/change/staged/children/StagedChildDetail.jsx
@@ -1,16 +1,24 @@
-import { useMemo } from 'react'
+import { use, useMemo } from 'react'
 import { Box } from '@mui/material'
 
 import FieldValueDisplay from '@/components/common/FieldValueDisplay'
 
+import ConceptContext from '@/contexts/panels/concepts/ConceptContext'
+
 import { drop } from '@/lib/utils'
+import { CONCEPT } from '@/lib/constants'
 
 const StagedChildDetail = ({ stagedChild }) => {
+  const { isMarineOrganism } = use(ConceptContext)
   const { updates } = stagedChild
 
   const fieldValues = useMemo(() => {
-    return Object.entries(drop(updates, ['name']))
-  }, [updates])
+    const hiddenFields = ['name']
+    if (!isMarineOrganism) {
+      hiddenFields.push(CONCEPT.FIELD.RANK_NAME, CONCEPT.FIELD.RANK_LEVEL)
+    }
+    return Object.entries(drop(updates, hiddenFields))
+  }, [isMarineOrganism, updates])
 
   return (
     <Box>
