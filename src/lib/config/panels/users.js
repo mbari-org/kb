69f90361838fcc06ebd0e9506b02ca0266b80f6e
Paul Rogers <paul@dingosky.com>
2026-07-30 20:27:58 +0200

    coalesce user panel JSON

--- src/lib/config/panels/users.js
+++ src/lib/config/panels/users.js
@@ -1,11 +1,7 @@
 import panel from '@/config/panels/users/panel.json'
-import { BUTTON } from '../button.js'
 import { MODALS } from './users/modals.js'
 
 export const USERS = {
-  BUTTON: {
-    ADD: BUTTON.ADD,
-  },
   MODALS,
   PANEL: panel,
 }
