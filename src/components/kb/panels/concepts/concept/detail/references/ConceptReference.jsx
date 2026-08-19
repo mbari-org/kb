a977d66641f7f0f96c866cdda0383e25b7a37e4e
Paul Rogers <paul@dingosky.com>
2026-08-19 18:45:14 +0200

    Add tooltip to Reference DOI
    And fix some project standard capitalization on other tooltips

--- src/components/kb/panels/concepts/concept/detail/references/ConceptReference.jsx
+++ src/components/kb/panels/concepts/concept/detail/references/ConceptReference.jsx
@@ -1,5 +1,9 @@
 import { Box, Typography } from '@mui/material'
 
+import KBTooltipTarget from '@/components/common/tooltip/KBTooltipTarget'
+
+import CONFIG from '@/lib/config'
+
 const ConceptReference = ({ reference }) => {
   if (!reference) {
     return null
@@ -19,26 +23,32 @@ const ConceptReference = ({ reference }) => {
       }}
       title={reference.citation}
     >
-      <Typography
-        component='a'
-        href={reference.doi}
-        noWrap
-        rel='noopener noreferrer'
-        sx={{
-          color: 'text.primary',
-          cursor: 'pointer',
-          display: 'block',
-          textDecoration: 'none',
-          '&:hover': {
-            color: 'primary.main',
-            textDecoration: 'none',
-          },
-        }}
-        target='_blank'
-        title={reference.citation}
+      <KBTooltipTarget
+        title={CONFIG.PANELS.CONCEPTS.REFERENCES.CITATION.TOOLTIP}
+        placement='top-start'
+        wrapperSx={{ display: 'block', width: '100%' }}
       >
-        {reference.citation}
-      </Typography>
+        <Typography
+          component='a'
+          href={reference.doi}
+          noWrap
+          rel='noopener noreferrer'
+          sx={{
+            color: 'text.primary',
+            cursor: 'pointer',
+            display: 'block',
+            textDecoration: 'none',
+            '&:hover': {
+              color: 'primary.main',
+              textDecoration: 'none',
+            },
+          }}
+          target='_blank'
+          title={reference.citation}
+        >
+          {reference.citation}
+        </Typography>
+      </KBTooltipTarget>
     </Box>
   )
 }
